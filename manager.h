b3abd269c9848c8e0c8e3f64a7b58a7a1e612ef1
18yk04 <90135853+18yk04@users.noreply.github.com>
2022-05-04 22:49:07 +0900

    create manager.h

---
+++ manager.h
@@ -0,0 +1,8 @@
+#include "time.h"
+
+int selectMenu();
+void listTime(Time *t, int count);
+void listPurpose(Time *t, int count);
+void selectDataNum(Time *t, int count);
+void saveData(Time *t, int count);
+int loadData(Time *t);
